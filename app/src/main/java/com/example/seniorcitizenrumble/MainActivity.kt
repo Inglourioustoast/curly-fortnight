82ba7f9f9fea9d305a12c0d091ffb3f68ec95cd7
jake.dove <Jake@AstraAppivate.co.uk>
2023-06-09 10:31:09 +0100

    added expand on click functionality

--- app/src/main/java/com/example/seniorcitizenrumble/MainActivity.kt
+++ app/src/main/java/com/example/seniorcitizenrumble/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.seniorcitizenrumble
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,17 +23,20 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
+
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import com.example.seniorcitizenrumble.ui.theme.SeniorCitizenRumbleTheme
 
 class MainActivity : AppCompatActivity() {
@@ -45,7 +53,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    data class Album(val artist:String, val name: String)
+    data class Album(
+        val artist:String,
+        val name: String,
+        val year: String = "")
 
     @Composable
     fun AlbumList(albums: List<Album>) {
@@ -58,21 +69,24 @@ class MainActivity : AppCompatActivity() {
 
     @Composable
     fun MessageCard(album: Album) {
-        Row(modifier = Modifier.padding(all = 8.dp))
-        {
+        Row(modifier = Modifier.padding(all = 8.dp).animateContentSize() ) {
             Image(
                 painter = painterResource(R.drawable._600w_1nr6gsundkw),
                 contentDescription = "no description",
                 modifier = Modifier
-                    // Set image size to 40 dp
                     .size(40.dp)
                     .clip(CircleShape)
-                    // Clip image to be shaped as a circle
                     .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
             )
             Spacer(modifier = Modifier.width(8.dp))
 
-            Column {
+            //variable to track if the message is exapnded
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            )
+
+            Column (modifier = Modifier.clickable { isExpanded = !isExpanded }) {
                 Text(
                     text = album.artist,
                     color = MaterialTheme.colorScheme.secondary,
@@ -80,8 +94,12 @@ class MainActivity : AppCompatActivity() {
                 )
 
                 Spacer(modifier = Modifier.height(4.dp))
-                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
-
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier.animateContentSize().padding(1.dp))
+                {
 
                     Text(
                         text = album.name,
@@ -89,9 +107,25 @@ class MainActivity : AppCompatActivity() {
                         style = MaterialTheme.typography.bodyMedium
                     )
                 }
+                if (isExpanded) {
+                    Column {
+                        Text(
+                            text = album.year,
+                            modifier = Modifier.padding(all = 4.dp),
+                            style = MaterialTheme.typography.bodyMedium,
+                        )
+                        AnimatedVisibility(
+                            visible = true,
+                            enter = fadeIn(),
+                            exit = fadeOut()
+                        ) {
+
+                        }
+                    }
+                    }
+                }
             }
         }
-    }
 
 
     @Preview(name = "Full Preview",
@@ -107,7 +141,18 @@ class MainActivity : AppCompatActivity() {
     fun PreviewGreeting() {
         SeniorCitizenRumbleTheme{
         Surface(modifier = Modifier.fillMaxSize()) {
-            AlbumList(listOf(Album("fleetwood Mac", "one album"), Album("Led Zepplin", "Another album")))
+            AlbumList(listOf(
+
+                Album(
+                    artist = "fleetwood Mac",
+                    name ="one album",
+                    year = "1988"),
+
+                Album(
+                    artist = "Led Zepplin",
+                    name = "Another album",
+                    year = "1988"),
+                ))
         }
     }
     }
